7d25b55259244891b6e0ab66d26b53dd9bdbc6df
Fabrizio Mesto <fabrizio.mesto@telntsrl.com>
2021-02-15 11:13:12 +0100

    remove admob

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-export * from './services/acquisti-validator.service';
-export * from './services/acquistoModel';
-export * from './services/admob.service';
 export * from './services/authentication.service';
 export * from './services/cache.service';
 export * from './services/https.service';
